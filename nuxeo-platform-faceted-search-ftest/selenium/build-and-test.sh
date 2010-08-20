7900c9850e7c002bd648be065092dd5186b8473d
Thomas Roger <troger@nuxeo.com>
2010-08-20 21:02:09 +0200

    NXP-5446: add selenium tests

---
+++ nuxeo-platform-faceted-search-ftest/selenium/build-and-test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+HERE=$(cd $(dirname $0); pwd -P)
+
+# Retrieve Nuxeo Distribution, nuxeo-platform-faceted-search jars and selenium-server.jar
+(cd .. && mvn -o clean dependency:copy) || exit 1
+
+# Start JBoss
+cd ../target
+unzip nuxeo-distribution-jboss-*.zip || exit 1
+mv nuxeo-dm-*-jboss jboss || exit 1
+mv nuxeo-platform-faceted-search* jboss/server/default/deploy/nuxeo.ear/system/ || exit 1
+chmod +x jboss/bin/nuxeoctl || exit 1
+jboss/bin/nuxeoctl start || exit 1
+
+# Run selenium tests
+cd $HERE
+./run.sh
+ret1=$?
+
+# Stop JBoss
+(cd ../target && jboss/bin/nuxeoctl stop) || exit 1
+
+# Exit if some tests failed
+[ $ret1 -eq 0 ] || exit 9
