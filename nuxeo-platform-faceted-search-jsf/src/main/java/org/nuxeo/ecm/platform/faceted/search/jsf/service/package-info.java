d28eea211b6a55136b6ee03e6b3b5be52737dd8f
Thomas Roger <troger@nuxeo.com>
2010-09-09 16:55:15 +0200

    README + javadoc

---
+++ nuxeo-platform-faceted-search-jsf/src/main/java/org/nuxeo/ecm/platform/faceted/search/jsf/service/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Service and related objects handling faceted searches.
+ * @since 5.4
+ */
+package org.nuxeo.ecm.platform.faceted.search.jsf.service;
---
+++ nuxeo-platform-faceted-search-jsf/src/main/java/org/nuxeo/ecm/platform/faceted/search/jsf/util/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Helper classes to work with JSON and documents' metadata.
+ * @since 5.4
+ */
+package org.nuxeo.ecm.platform.faceted.search.jsf.util;
