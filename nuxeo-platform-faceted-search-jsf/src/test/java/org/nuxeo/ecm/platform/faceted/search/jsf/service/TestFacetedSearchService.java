32e2f49c9946a1d03c94b917a05d756df89a94ff
Florent Guillaume <fg@nuxeo.com>
2015-02-16 18:40:51 +0100

    NXP-16535: use annotations from javax.inject instead of guice

--- nuxeo-platform-faceted-search-jsf/src/test/java/org/nuxeo/ecm/platform/faceted/search/jsf/service/TestFacetedSearchService.java
+++ nuxeo-platform-faceted-search-jsf/src/test/java/org/nuxeo/ecm/platform/faceted/search/jsf/service/TestFacetedSearchService.java
@@ -31,6 +31,7 @@ import java.util.List;
 import java.util.Set;
 
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -57,8 +58,6 @@ import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.test.runner.LocalDeploy;
-
-import com.google.inject.Inject;
 
 /**
  * @author <a href="mailto:troger@nuxeo.com">Thomas Roger</a>
